e663b77b8c2628f4c0090acc876516546529fb76
Catherine Dean <catherinemdean15@gmail.com>
2020-10-20 09:16:36 -0600

    add tests for calling all info hash keys, add renter method tests

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -20,5 +20,26 @@ class ApartmentTest < Minitest::Test
   def test_it_calls_number
     assert_equal "A1", @unit1.number
   end
-  
+
+  def test_it_calls_monthly_rent
+    assert_equal 1200, @unit1.monthly_rent
+  end
+
+  def test_it_calls_bathrooms
+    assert_equal 1, @unit1.bathrooms
+  end
+
+  def test_it_calls_bedrooms
+    assert_equal 1, @unit1.bedrooms
+  end
+
+  def test_it_calls_renter_when_no_renter
+    assert_equal nil, @unit1.renter
+  end
+
+  def test_it_calls_renter_when_renter
+    @unit1.add_renter(@renter1)
+    assert_equal @renter1, @unit1.renter
+  end 
+
 end
