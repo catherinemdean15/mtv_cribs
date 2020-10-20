644bcbc9560a67447d9b1a51742682ca68dc5bc2
Catherine Dean <catherinemdean15@gmail.com>
2020-10-20 09:39:59 -0600

    add average_rent test

--- test/building_test.rb
+++ test/building_test.rb
@@ -48,4 +48,10 @@ class BuildingTest < Minitest::Test
     assert_equal ["Aurora", "Tim"], @building.renters
   end
 
+  def test_average_rent
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    assert_equal 1099.5, @building.average_rent
+  end 
+
 end
